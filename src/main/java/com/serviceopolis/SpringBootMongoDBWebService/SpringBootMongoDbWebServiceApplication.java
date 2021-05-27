d18e736f4e71e051e13931db8570b635f3ba3454
LeanBlauth <le4ndroblauth@gmail.com>
2021-05-26 22:39:52 -0300

    First commit; project created

---
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/SpringBootMongoDbWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.serviceopolis.SpringBootMongoDBWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDbWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviceopolis/SpringBootMongoDBWebService/SpringBootMongoDbWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceopolis.SpringBootMongoDBWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
