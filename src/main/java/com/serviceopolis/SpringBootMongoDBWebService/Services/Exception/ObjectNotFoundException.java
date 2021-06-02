9e88253fe831237ff6cd93acba01f532a581a415
LeanBlauth <le4ndroblauth@gmail.com>
2021-06-01 23:02:31 -0300

    User resource and service layers: findById method

---
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Services/Exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.serviceopolis.SpringBootMongoDBWebService.Services.Exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
--- src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Services/UserService.java
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Services/UserService.java
@@ -1,12 +1,14 @@
 package com.serviceopolis.SpringBootMongoDBWebService.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.serviceopolis.SpringBootMongoDBWebService.Domain.User;
 import com.serviceopolis.SpringBootMongoDBWebService.Repository.UserRepository;
+import com.serviceopolis.SpringBootMongoDBWebService.Services.Exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String Id) {
+		Optional<User> obj = repo.findById(Id);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
