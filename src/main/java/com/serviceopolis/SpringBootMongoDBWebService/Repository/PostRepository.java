7d65f28430749262bda837af76ba42079e056f70
LeanBlauth <le4ndroblauth@gmail.com>
2021-06-04 18:27:17 -0300

    Post search with @Query

--- src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Repository/PostRepository.java
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.serviceopolis.SpringBootMongoDBWebService.Repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.serviceopolis.SpringBootMongoDBWebService.Domain.Post;
@@ -11,5 +12,8 @@ import com.serviceopolis.SpringBootMongoDBWebService.Domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
