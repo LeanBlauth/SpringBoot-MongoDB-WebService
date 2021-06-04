4205d4956df58b095dfa56e268f6f3f2840e0419
LeanBlauth <le4ndroblauth@gmail.com>
2021-06-04 16:01:44 -0300

    Coupling of user to post through author DTO

--- src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Config/Instantiation.java
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.serviceopolis.SpringBootMongoDBWebService.DTO.AuthorDTO;
 import com.serviceopolis.SpringBootMongoDBWebService.Domain.Post;
 import com.serviceopolis.SpringBootMongoDBWebService.Domain.User;
 import com.serviceopolis.SpringBootMongoDBWebService.Repository.PostRepository;
@@ -36,10 +37,12 @@ public class Instantiation  implements CommandLineRunner {
 		User plumbus = new User(null, "Plumbus Gorgonoid", "gorgon@gmail.com");
 		User uranus = new User(null, "Uranus Darlington", "uranus@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "It's so coooldd !", "It's so cold my hooves are freezing", rudolph);
-		Post post2 = new Post(null, sdf.parse("14/11/2035"), "where is my pizza", "It has been 2 hours already I'm hungry you know", plumbus);
-		
 		userRepo.saveAll(Arrays.asList(rudolph, plumbus, uranus));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "It's so coooldd BAA !", "It's so cold my hooves are freezing", new AuthorDTO(rudolph));
+		Post post2 = new Post(null, sdf.parse("14/11/2035"), "where is my pizza", "It has been 2 hours already I'm hungry you know", new AuthorDTO(plumbus));
+		
+		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
