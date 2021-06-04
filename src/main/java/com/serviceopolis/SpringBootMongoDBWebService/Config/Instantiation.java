a2deba5aaf54adc3962968b9c2afd8cc738de956
LeanBlauth <le4ndroblauth@gmail.com>
2021-06-04 17:33:23 -0300

    Adding comments to the posts

--- src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Config/Instantiation.java
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.serviceopolis.SpringBootMongoDBWebService.DTO.AuthorDTO;
+import com.serviceopolis.SpringBootMongoDBWebService.DTO.CommentDTO;
 import com.serviceopolis.SpringBootMongoDBWebService.Domain.Post;
 import com.serviceopolis.SpringBootMongoDBWebService.Domain.User;
 import com.serviceopolis.SpringBootMongoDBWebService.Repository.PostRepository;
@@ -40,7 +41,14 @@ public class Instantiation  implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "It's so coooldd BAA !", "It's so cold my hooves are freezing", new AuthorDTO(rudolph));
 		Post post2 = new Post(null, sdf.parse("14/11/2035"), "where is my pizza", "It has been 2 hours already I'm hungry you know", new AuthorDTO(plumbus));
-				
+		
+		CommentDTO c1 = new CommentDTO("Dont't forget your hooves' boots !", sdf.parse("22/03/2035"), new AuthorDTO(uranus));
+		CommentDTO c2 = new CommentDTO("Damn it happened to me last week it pisses me off !", sdf.parse("14/11/2035"), new AuthorDTO(rudolph));
+		CommentDTO c3 = new CommentDTO("Poor Rudolph T___T", sdf.parse("22/03/2035"), new AuthorDTO(plumbus));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c3));
+		post2.getComments().addAll(Arrays.asList(c2));
+		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 		
 		rudolph.getPosts().addAll(Arrays.asList(post1));
