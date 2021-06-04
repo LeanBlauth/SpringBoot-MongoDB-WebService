cf18247c30e43d70397a14b78ab71f800f384ee8
LeanBlauth <le4ndroblauth@gmail.com>
2021-06-04 17:06:32 -0300

    Post resource and service layers: findById

---
+++ src/main/java/com/serviceopolis/SpringBootMongoDBWebService/Services/PostService.java
@@ -0,0 +1,23 @@
+package com.serviceopolis.SpringBootMongoDBWebService.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.serviceopolis.SpringBootMongoDBWebService.Domain.Post;
+import com.serviceopolis.SpringBootMongoDBWebService.Repository.PostRepository;
+import com.serviceopolis.SpringBootMongoDBWebService.Services.Exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String Id) {
+		Optional<Post> obj = repo.findById(Id);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+}
